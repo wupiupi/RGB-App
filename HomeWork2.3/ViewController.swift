4dc73919f694a27498e21398ef7cf8ee0287e91e
Paul <pasha.makey@gmail.com>
2023-11-16 23:55:49 +0300

    Initial Commit

---
+++ HomeWork2.3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork2.3
+//
+//  Created by Paul Makey on 16.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
