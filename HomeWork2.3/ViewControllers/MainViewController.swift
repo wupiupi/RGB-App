fa37b06f6d1082edaee820fc2f112af47547259f
Paul <pasha.makey@gmail.com>
2023-12-08 20:24:52 +0300

    Getting delegate from settingsVC

--- HomeWork2.3/ViewControllers/MainViewController.swift
+++ HomeWork2.3/ViewControllers/MainViewController.swift
@@ -1,17 +1,32 @@
 //
 //  MainViewController.swift
-//  HomeWork2.3
+//  RGB_App
 //
 //  Created by Paul Makey on 8.12.23.
 //
 
 import UIKit
 
+protocol SettingsViewControllerDelegate: AnyObject {
+    func setColor(with view: UIView)
+}
+
 final class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingsVC = segue.destination as? SettingsViewController else { return }
+        settingsVC.colorView = view
+        settingsVC.delegate = self
+    }
+}
 
-        
+extension MainViewController: SettingsViewControllerDelegate {
+    func setColor(with view: UIView) {
+        self.view.backgroundColor = view.backgroundColor
     }
 }
