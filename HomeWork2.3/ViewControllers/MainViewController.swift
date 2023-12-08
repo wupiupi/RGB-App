c6a7f092d3f9792668a6f003f32c62fe36baae88
Paul <pasha.makey@gmail.com>
2023-12-08 18:58:41 +0300

    New class added

---
+++ HomeWork2.3/ViewControllers/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  HomeWork2.3
+//
+//  Created by Paul Makey on 8.12.23.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
